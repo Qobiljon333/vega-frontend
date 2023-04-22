67c19cc2c510d6ac4ad5600f63c9c0bb45572224
Qobiljon333 <95879507+Qobiljon333@users.noreply.github.com>
2023-04-22 18:16:56 +0500

    first full app to use with server  url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 const mainURL = axios.create({
+    baseURL: "https://vega-backend-first-ready-app.onrender.com"
     // baseURL: "https://vega-backend-g07s.onrender.com/"
     // baseURL: "https://vega-backend-qobuljon.vercel.app/"
-    baseURL: "http://localhost:8000/"
+    // baseURL: "http://localhost:8000/"
     // baseURL: "https://mustang-backend.herokuapp.com"
 })
 export default mainURL
